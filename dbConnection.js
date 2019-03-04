bca401c66f2b690669bbb304ed32c0b0d84a5ec1
Denis Niwemugisha <denisniwemugisha@Deniss-MacBook-Pro-2.local>
2019-03-04 15:05:23 +0200

    add database_url

--- dbConnection.js
+++ dbConnection.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 
         let sequelize = new Sequelize('dd4fpkk831ece2', 'lpisfweczetfaz', 'f8358480c1f3ba4f2def6351e774123725d59fade028875c4e743a6323e47b05', {
-            host: 'c2-23-21-128-35.compute-1.amazonaws.com',
+            host: 'ec2-23-21-128-35.compute-1.amazonaws.com',
             dialect: 'postgres',
             operatorsAliases: false,
         
